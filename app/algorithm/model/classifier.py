4031f307a27d1e110e42b4b68437595116ba4a3e
Abu Desai <abhyuday.desai@gmail.com>
2023-01-01 13:27:09 -0800

    change to predict; change to json infer; other cleanup

--- app/algorithm/model/classifier.py
+++ app/algorithm/model/classifier.py
@@ -25,7 +25,9 @@ class Classifier():
         
         
     def build_model(self): 
-        model = SVC(C = self.C, degree = self.degree, tol = self.tol, kernel = self.kernel)
+        model = SVC(C = self.C, degree = self.degree, tol = self.tol, 
+            probability=True,
+            kernel = self.kernel)
         return model
     
     
@@ -34,8 +36,11 @@ class Classifier():
         
     
     def predict(self, X, verbose=False): 
-        preds = self.model.predict(X)
-        return preds 
+        return self.model.predict(X)          
+        
+    
+    def predict_proba(self, X, verbose=False): 
+        return self.model.predict_proba(X) 
     
 
     def summary(self):
@@ -49,29 +54,21 @@ class Classifier():
 
     
     def save(self, model_path): 
-        joblib.dump(self, os.path.join(model_path, model_fname))
-        
+        joblib.dump(self, os.path.join(model_path, model_fname))        
 
 
     @classmethod
     def load(cls, model_path):         
-        SVclassifier = joblib.load(os.path.join(model_path, model_fname))
-        # print("where the load function is getting the model from: "+ os.path.join(model_path, model_fname))        
-        return SVclassifier
+        model = joblib.load(os.path.join(model_path, model_fname))
+        return model
 
 
 def save_model(model, model_path):
-    # print(os.path.join(model_path, model_fname))
-    joblib.dump(model, os.path.join(model_path, model_fname)) #this one works
-    # print("where the save_model function is saving the model to: " + os.path.join(model_path, model_fname))
-    
+    model.save(model_path)
+
 
 def load_model(model_path): 
-    try: 
-        model = joblib.load(os.path.join(model_path, model_fname))   
-    except: 
-        raise Exception(f'''Error loading the trained {MODEL_NAME} model. 
-            Do you have the right trained model in path: {model_path}?''')
+    model = joblib.load(os.path.join(model_path, model_fname))   
     return model
 
 
